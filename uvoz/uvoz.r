8f6b6306f815c4597b631db81dffc243dbe29040
Anja Plesec <anja.plesec@student.fmf.uni-lj.si>
2018-12-27 13:53:25 +0100

    popravila nekatere uvožene tabele

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -12,17 +12,16 @@ library(ggplot2)
 
 #uvoz brezposelnosti izobrazba
 uvozi.brezposelnost_izo <- function(izobrazba) {
-  stolpci <- c("regija", "spol", "leto", "Brez izobrazbe", "Osnovnošolska", "Nižja ali srednja poklicna", 
+  stolpci <- c("regija", "leto", "spol", "Brez izobrazbe", "Osnovnošolska", "Nižja ali srednja poklicna", 
                "Srednja strokovna, splošna" , "Višješolska, visokošolska")
   podatki <- read_csv2("podatki/brazposelnost_izo.csv", 
                        col_names=stolpci,
                        locale=locale(encoding="Windows-1250"),
-                       skip=7, n_max=10) %>% .[, -(1:2)]  %>%
-    mutate(`Brez izobrazbe`=parse_number(`Brez izobrazbe`)) %>%
-    melt(id.vars="leto", variable.name="izobrazba", value.name="stevilo")
-  return(podatki)
+                       skip=6, n_max=30) %>% .[, -(1:1)] %>% 
+  melt(id.vars= "leto",  variable.name=c("izobrazba", "spol"), value.name="stevilo")
 }
 brezposelnost_izo <- uvozi.brezposelnost_izo()
+
 
 #uvoz brezposelnosti
 uvozi.brezposelni <- function(ljudje) {
@@ -37,21 +36,6 @@ uvozi.brezposelni <- function(ljudje) {
 
 brezposelni <- uvozi.brezposelni()
 
-#uvoz brezposelnosti glede na statistične regije
-uvozi.statistične_regije <- function(regije) {
-  stolpci <- c("regija", "2008", "2009", "2010", "2011", "2012", "2013","2014",
-               "2015","2016","2017")
-  podatki <- read_csv2("podatki/statistične_regije.csv", 
-                       col_names=stolpci,
-                       locale=locale(encoding="Windows-1250"),
-                       skip=4, n_max=12) %>%
-    melt(id.vars="regija", variable.name="leto", value.name="stevilo")
-}
-
-statistične_regije <- uvozi.statistične_regije()
-
-
-
 
 #uvoz brezposelnosti glede na starost in spol
 uvozi.brezposelnost_starost <- function(starost) {
@@ -60,11 +44,26 @@ uvozi.brezposelnost_starost <- function(starost) {
   podatki <- read_csv2("podatki/brezposelnost_starost.csv",
                        col_names=stolpci,
                        locale=locale(encoding="Windows-1250"),
-                       skip=6, n_max=11) %>% .[, -(1:1)] 
+                       skip=6, n_max=9) %>% .[, -(1:1)] %>% 
+    melt(id.vars= c("starost", "spol"),  variable.name="leta", value.name="stevilo")
   return(podatki)
 }
 
 brezposelnost_starost <- uvozi.brezposelnost_starost()
+
+
+#uvoz brezposelnosti glede na statistične regije
+uvozi.statistične_regije <- function(regije) {
+  stolpci <- c("regija", "2008", "2009", "2010", "2011", "2012", "2013","2014",
+               "2015","2016","2017")
+  podatki <- read_csv2("podatki/statisticne_regije.csv", 
+                       col_names=stolpci,
+                       locale=locale(encoding="Windows-1250"),
+                       skip=4, n_max=12) %>%
+    melt(id.vars="regija", variable.name="leto", value.name="stevilo")
+}
+
+statistične_regije <- uvozi.statistične_regije()
 
 
 #uvoz brezposelnosti glede na tip gospodinjstva
@@ -80,13 +79,29 @@ tip_gospodinjstva <- uvozi.tip_gospodinjstva()
 
 #uvoz brezposelnosti glede na trajanje brezposelnosti
 uvozi.trajanje_brezposelnosti <- function(trajanje) {
-  stolpci <- c("trajanje", "spol", "gospodinjstvo", "število")
+  stolpci <- c("l", "trajanje", "spol", "2008", "2009", "2010", "2011", "2012", "2013","2014",
+               "2015","2016","2017")
   podatki <- read_csv2("podatki/trajanje_brezposelnosti.csv", 
                        col_names=stolpci,
                        locale=locale(encoding="Windows-1250"),
-                       skip=6, n_max=15) %>% .[, -(1:1)] 
+                       skip=6, n_max=15) %>% .[, -(1:1)] %>% 
+  melt(id.vars= c("trajanje", "spol"),  variable.name="leta", value.name="stevilo")
 }
 
 trajanje_brezposelnosti <- uvozi.trajanje_brezposelnosti()
+
+
+#uvoz brezposelnosti glede na države
+uvozi.brezposelnost_drzave <- function(drzava) {
+  stolpci <- c("država", "2008", "2009", "2010", "2011", "2012", "2013","2014",
+               "2015","2016","2017")
+  podatki <- read_csv2("podatki/brezposelnost_drzave.csv", 
+                       col_names=stolpci,
+                       locale=locale(encoding="Windows-1250"),
+                       skip=11, n_max=34) %>%
+  melt(id.vars="država", variable.name="leta", value.name="stevilo")
+}
+
+brezposelnost_drzave <- uvozi.brezposelnost_drzave()
 
 
