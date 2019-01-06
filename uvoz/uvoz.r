6417f71aaa21feacf39b7114564f1a430d76186b
Anja Plesec <ancyplesec8gmail.com>
2019-01-06 18:09:13 +0100

    začetek zemljevidov

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -9,24 +9,24 @@ library(dplyr)
 library(readr)
 library(ggplot2)
 
-
+#!!!!!!!!!
 #uvoz brezposelnosti izobrazba
 uvozi.brezposelnost_izo <- function(izobrazba) {
-  stolpci <- c("regija", "leto", "spol", "Brez izobrazbe", "Osnovnošolska", "Nižja ali srednja poklicna", 
-               "Srednja strokovna, splošna" , "Višješolska, visokošolska")
+  stolpci <- c("regija", "leto", "spol", "Brez izobrazbe", "Osnovnošolska", 
+               "Nižja ali srednja poklicna", "Srednja strokovna, splošna" , 
+               "Višješolska, visokošolska")
   podatki <- read_csv2("podatki/brazposelnost_izo.csv", 
                        col_names=stolpci,
                        locale=locale(encoding="Windows-1250"),
                        skip=6, n_max=30) %>% .[, -(1:1)] %>% 
-  melt(id.vars= "leto",  variable.name=c("izobrazba", "spol"), value.name="stevilo")
+    melt(id.vars= c("leto", "spol"),  variable.name="izobrazba", value.name="stevilo")
 }
-brezposelnost_izo <- uvozi.brezposelnost_izo()
 
+brezposelnost_izo <- uvozi.brezposelnost_izo()
 
 #uvoz brezposelnosti
 uvozi.brezposelni <- function(ljudje) {
-  stolpci <- c("j", "p","spol", "2008", "2009", "2010", "2011", "2012", "2013","2014",
-               "2015","2016","2017")
+  stolpci <- c("j", "p","spol", 2008:2017)
   podatki <- read_csv2("podatki/brezposelni.csv", 
                        col_names=stolpci,
                        locale=locale(encoding="Windows-1250"),
@@ -36,17 +36,16 @@ uvozi.brezposelni <- function(ljudje) {
 
 brezposelni <- uvozi.brezposelni()
 
-
+#!!!!!
 #uvoz brezposelnosti glede na starost in spol
 uvozi.brezposelnost_starost <- function(starost) {
-  stolpci <- c("ime", "starost", "spol", "2008", "2009", "2010", "2011", "2012", "2013","2014",
-               "2015","2016","2017")
+  stolpci <- c("ime", "starost", "spol", 2008:2017)
   podatki <- read_csv2("podatki/brezposelnost_starost.csv",
                        col_names=stolpci,
                        locale=locale(encoding="Windows-1250"),
                        skip=6, n_max=9) %>% .[, -(1:1)] %>% 
-    melt(id.vars= c("starost", "spol"),  variable.name="leta", value.name="stevilo")
-  return(podatki)
+  melt(id.vars= c("starost", "spol"),  variable.name="leta", value.name="stevilo") %>%
+  mutate(stevilo = parse_number(stevilo))
 }
 
 brezposelnost_starost <- uvozi.brezposelnost_starost()
@@ -54,33 +53,34 @@ brezposelnost_starost <- uvozi.brezposelnost_starost()
 
 #uvoz brezposelnosti glede na statistične regije
 uvozi.statistične_regije <- function(regije) {
-  stolpci <- c("regija", "2008", "2009", "2010", "2011", "2012", "2013","2014",
-               "2015","2016","2017")
+  stolpci <- c("regija", 2008:2017)
   podatki <- read_csv2("podatki/statisticne_regije.csv", 
                        col_names=stolpci,
                        locale=locale(encoding="Windows-1250"),
                        skip=4, n_max=12) %>%
-    melt(id.vars="regija", variable.name="leto", value.name="stevilo")
+  melt(id.vars="regija", variable.name="leto", value.name="stevilo")  %>%
+  mutate(stevilo = parse_number(stevilo))
 }
 
 statistične_regije <- uvozi.statistične_regije()
 
-
+#!!!
 #uvoz brezposelnosti glede na tip gospodinjstva
 uvozi.tip_gospodinjstva <- function(gopodinjstvo) {
-  stolpci <- c("regija", "leto", "gospodinjstvo", "število")
+  stolpci <- c("regija", "leto", "gospodinjstvo", "stevilo")
   podatki <- read_csv2("podatki/tip_gospodinjstva.csv", 
                        col_names=stolpci,
                        locale=locale(encoding="Windows-1250"),
-                       skip=5, n_max=49) %>% .[, -(1:1)] 
+                       skip=5, n_max=49) %>% .[, -(1:1)] %>%
+  melt(id.vars="gospodinjstvo", variable.name="leto", value.name="stevilo")
 }
+
 tip_gospodinjstva <- uvozi.tip_gospodinjstva()
 
-
+#!!!!
 #uvoz brezposelnosti glede na trajanje brezposelnosti
 uvozi.trajanje_brezposelnosti <- function(trajanje) {
-  stolpci <- c("l", "trajanje", "spol", "2008", "2009", "2010", "2011", "2012", "2013","2014",
-               "2015","2016","2017")
+  stolpci <- c("l", "trajanje", "spol", 2008:2017)
   podatki <- read_csv2("podatki/trajanje_brezposelnosti.csv", 
                        col_names=stolpci,
                        locale=locale(encoding="Windows-1250"),
@@ -93,8 +93,7 @@ trajanje_brezposelnosti <- uvozi.trajanje_brezposelnosti()
 
 #uvoz brezposelnosti glede na države
 uvozi.brezposelnost_drzave <- function(drzava) {
-  stolpci <- c("država", "2008", "2009", "2010", "2011", "2012", "2013","2014",
-               "2015","2016","2017")
+  stolpci <- c("država", 2008:2017)
   podatki <- read_csv2("podatki/brezposelnost_drzave.csv", 
                        col_names=stolpci,
                        locale=locale(encoding="Windows-1250"),
@@ -104,4 +103,5 @@ uvozi.brezposelnost_drzave <- function(drzava) {
 
 brezposelnost_drzave <- uvozi.brezposelnost_drzave()
 
-
+write.csv2(uvozi.brezposelnost_drzave,'podatki/tidy_brezposelni_drzave.csv', fileEncoding = 'UTF-8')
+write.csv2(uvozi.statistične_regije ,'podatki/tidy_statisticne_regije.csv', fileEncoding = 'UTF-8')
--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,13 +1,27 @@
 # 3. faza: Vizualizacija podatkov
+library(rgdal)
+library(dplyr)
+library(readr)
+library(ggplot2)
+library(digest)
+library(mosaic)
+
 
 # Uvozimo zemljevid.
-zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip", "OB",
-                             pot.zemljevida="OB", encoding="Windows-1250")
-levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
-  { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
-zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels=levels(obcine$obcina))
-zemljevid <- fortify(zemljevid)
+#zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip", "OB",
+#                             pot.zemljevida="OB", encoding="Windows-1250")
+#levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
+#  { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
+#zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels=levels(obcine$obcina))
+#zemljevid <- fortify(zemljevid)
 
 # Izračunamo povprečno velikost družine
-povprecja <- druzine %>% group_by(obcina) %>%
-  summarise(povprecje=sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
+#povprecja <- druzine %>% group_by(obcina) %>%
+#  summarise(povprecje=sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
+
+slovenija <- uvozi.zemljevid("https://biogeo.ucdavis.edu/data/gadm3.6/shp/gadm36_SVN_shp.zip", 
+                             "regije_slovenija") %>% fortify()
+
+regije <- filter(slovenija, long < 80 & lat < 85)
+
+ggplot() + geom_polygon(data=regije, aes(x=long, y=lat, group=group, fill=id)) 
