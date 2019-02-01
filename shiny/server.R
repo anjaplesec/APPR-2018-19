7fd19ac9de942f47ae68567a9fb25b854aa09b2e
Anja Plesec <ancyplesec8gmail.com>
2019-02-01 14:04:12 +0100

    shiny deluje

--- shiny/server.R
+++ shiny/server.R
@@ -1,13 +1,24 @@
 library(shiny)
+library("readr")
+
+if(grepl("shiny",getwd())){setwd("..")}
+source("uvoz/uvoz.r")
+source("lib/libraries.r")
+
 
 function(input, output) {
-  
-   output$brezposelnost <- renderPlot({
-     tabela1 <- zadnja_faza %>% filter(obcina == input$obcina)
-     print(ggplot(tabela1, aes(x = leta, y = stevilo)) + geom_line() + 
-      ylab("Brezposelnost") + xlab("Leto") +
-      ggtitle("Brezposelnost po občinah"))
+  output$izborTabPanel = renderUI({
+    tabPanel("Graf",
+             sidebarPanel(
+               selectInput("obcina", label = "Izberite občino:",
+                           choices= (sort(unique(zadnja_faza$obcina))))),
+             mainPanel(plotOutput("brezposelnost")))
   })
+  
+  output$brezposelnost <- renderPlot({
+    tabela1 <- zadnja_faza %>% filter(obcina == input$obcina)
+    print(ggplot(tabela1) + geom_line(aes(x = leta, y = stevilo), group=1) + 
+            ylab("Brezposelnost") + xlab("Leto") +
+            ggtitle("Brezposelnost po občinah"))
+  }) 
 }
-    
- 
--- shiny/ui.R
+++ shiny/ui.R
@@ -1,10 +1,5 @@
 library(shiny)
-
 fluidPage(
   titlePanel("Brezposelnost po občinah"),
-  tabPanel("Graf",
-           sidebarPanel(
-             selectInput("obcina", label = "Izberite občino:",
-                         choices= (sort(unique(zadnja_faza$obcina))))),
-           mainPanel(plotOutput("brezposelnost")))
+  uiOutput("izborTabPanel")
 )
